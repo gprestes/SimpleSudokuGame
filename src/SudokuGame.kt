b08150adcbbc852f3c28d49dea2c410bade87ee9
Guilherme Prestes <gprestes@thoughtworks.com>
2018-04-10 16:34:57 -0300

    Changes createSolution method to return from file instead of from the code

--- src/SudokuGame.kt
+++ src/SudokuGame.kt
@@ -1,3 +1,5 @@
+import java.io.File
+
 /**
  * The Sudoku logic.
  */
@@ -26,22 +28,18 @@ class SudokuGame {
     // This property holds a 2D array of booleans. It is used to show or hide a value from our Sudoku board.
     val visibleElements = initVisibleElements()
 
-    // ## STEP_2 ##
-    // Update the body of this function to populate the array with the values read from "sudoku_1" file.
-    // Run the application again and confirm it behaves the same as before.
     // ## STEP_4 ##
     // Rename this function to "readSudokuBoard".
     private fun createSolution(): Array<IntArray> {
-        return arrayOf(
-                intArrayOf(3, 5, 9, 6, 1, 8, 4, 2, 7),
-                intArrayOf(7, 4, 2, 5, 3, 9, 8, 6, 1),
-                intArrayOf(1, 6, 8, 4, 7, 2, 9, 5, 3),
-                intArrayOf(4, 2, 3, 8, 9, 5, 7, 1, 6),
-                intArrayOf(5, 8, 7, 1, 6, 4, 3, 9, 2),
-                intArrayOf(6, 9, 1, 7, 2, 3, 5, 8, 4),
-                intArrayOf(2, 7, 5, 9, 4, 6, 1, 3, 8),
-                intArrayOf(8, 3, 4, 2, 5, 1, 6, 7, 9),
-                intArrayOf(9, 1, 6, 3, 8, 7, 2, 4, 5))
+        val sudokuFile = File("./src/sudoku_1")
+
+        val sudokuArray = Array(9) { IntArray(9) }
+
+        sudokuFile.readLines().forEachIndexed { index, line ->
+            sudokuArray[index] = line.split(" ").map { it.toInt() }.toIntArray()
+        }
+
+        return sudokuArray
     }
 
     // ## STEP_6 ##
