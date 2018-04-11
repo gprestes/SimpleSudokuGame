9ebe5cb4e55202c14bea16b199a45627fd407b43
Guilherme Prestes <gprestes@thoughtworks.com>
2018-04-11 17:28:41 -0300

    Adds the first test (finally)

---
+++ src/SudokuGameTest.kt
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test
+import kotlin.test.assertTrue
+
+internal class SudokuGameTest {
+
+    private val sudokuGame = SudokuGame()
+
+    @Test
+    fun isUserInputCorrectFor3InRow0Column0() {
+        val result = sudokuGame.isUserInputCorrect(3, 0, 0)
+
+        assertTrue { result }
+    }
+}
